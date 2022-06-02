9a993e5f126b8ea4215953da66b862deba0a61e9
SahidVhora <106720959+SahidVhora@users.noreply.github.com>
2022-06-02 14:35:20 -0400

    Update Card.java

--- src/cardtrickice1/Card.java
+++ src/cardtrickice1/Card.java
@@ -1,4 +1,4 @@
-/*
+/*STUDENT ID: 991662745
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -6,13 +6,11 @@
 package cardtrickice1;
 
 /** define card value and suits
- * 
- *
- * @author sivagamasrinivasan
+ * @author Mohammedsaheed Vhora
  */
 public class Card 
 {
-    private int value;
+    private int val;
     private String suits; //encapsulation
     //constant
     public static final String [] SUITS = { "diamonds","clubs","spades","hearts"};
@@ -20,32 +18,40 @@ public class Card
     /**
      * @return the value
      */
-    public int getValue() {
-        return value;
+    public int getallValue() {
+        return val;
     }
 
     /**
      * @param value the value to set
      */
-    public void setValue(int value) {
-        this.value = value;
+    public void setallValue(int val) {
+        this.val = val;
     }
 
     /**
      * @return the suits
      */
-    public String getSuits() {
+    public String getallSuits() {
         return suits;
     }
 
     /**
      * @param suits the suits to set
      */
-    public void setSuits(String suits) {
+    public void setallSuits(String suits) {
         this.suits = suits;
     }
    //number
     
     //method for suits
+
+    int generateRandomValue() {
+        return (int)(Math.random()*(12-1+1)+1);
+    }
+
+    String generateRandomSuit() {
+        return SUITS[(int)(Math.random()*(3-0+1)+0)];
+    }
     
 }
